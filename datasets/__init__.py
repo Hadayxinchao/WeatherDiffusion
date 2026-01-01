cec4ed4efc1125f9cd6fd46a6713486a0eef508a
Hoang Ha <SmithMerch2301@gmail.com>
2026-01-02 00:05:27 +0700

    Refactor dataset loading to improve clarity and error handling

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,8 +1,25 @@
-from datasets.allweather import *
-from datasets.snow100k import *
-from datasets.outdoorrain import *
-from datasets.raindrop import *
-from datasets.ohaze import *
-from datasets.custom_haze import *
+from .snow100k import Snow100K
+from .raindrop import RainDrop
+from .outdoorrain import OutdoorRain
+from .ohaze import OHaze
+from .custom_haze import CustomHaze
+from .allweather import AllWeather
 
-__all__ = ["AllWeather", "Snow100K", "OutdoorRain", "RainDrop", "OHaze", "CustomHaze"]
+def get_dataset(args, config):
+    dataset_name = config.data.dataset
+    
+    if dataset_name == 'Snow100K':
+        return Snow100K(args, config)
+    elif dataset_name == 'RainDrop':
+        return RainDrop(args, config)
+    elif dataset_name == 'OutdoorRain':
+        return OutdoorRain(args, config)
+    elif dataset_name == 'OHaze':
+        return OHaze(args, config)
+    elif dataset_name == 'AllWeather':
+        return AllWeather(args, config)
+    elif dataset_name.startswith('CustomHaze'):
+        return CustomHaze(args, config)
+    
+    else:
+        raise KeyError(f"Dataset không hợp lệ: {dataset_name}. Hãy kiểm tra lại configs.")
